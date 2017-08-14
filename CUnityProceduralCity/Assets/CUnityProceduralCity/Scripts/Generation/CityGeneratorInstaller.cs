bcc1fe867405d87ce94febcff77416b3036700ce
Casper <cmvanb@gmail.com>
2017-08-14 23:36:41 +0200

    improve file structure

--- CUnityProceduralCity/Assets/CUnityProceduralCity/Scripts/Generation/CityGeneratorInstaller.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using Zenject;
-
-public class CityGeneratorInstaller : Installer<CityGeneratorInstaller>
-{
-    public override void InstallBindings()
-    {
-    }
-}
---
+++ src/Assets/ProceduralCity/Scripts/Generation/CityGeneratorInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+namespace AltSrc.ProceduralCity.Generation
+{
+    public class CityGeneratorInstaller : Installer<CityGeneratorInstaller>
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
