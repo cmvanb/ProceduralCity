e48464bffded50ebcc0e9f0ee59ac69f369e3321
Casper <cmvanb@gmail.com>
2018-01-30 17:17:21 +0100

    implement camera controller for example

---
+++ src/Assets/ProceduralCity/Scripts/Example01/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AltSrc.UnityCommon.Math;
+
+namespace AltSrc.ProceduralCity.Generation
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        protected float zoomFactor = 1200f;
+
+        [SerializeField]
+        protected bool zoomReverse = false;
+
+        [SerializeField]
+        protected float movementFactor = 30f;
+
+        protected void Update()
+        {
+            float horizontalAxis = Input.GetAxisRaw("Horizontal");
+            float scrollWheelAxis = -Input.GetAxis("Mouse ScrollWheel");
+            float verticalAxis = Input.GetAxisRaw("Vertical");
+
+            transform.position += new Vector3(
+                horizontalAxis * movementFactor, 
+                scrollWheelAxis * zoomFactor * (zoomReverse ? -1f : 1f), 
+                verticalAxis * movementFactor);
+        }
+    }
+}
