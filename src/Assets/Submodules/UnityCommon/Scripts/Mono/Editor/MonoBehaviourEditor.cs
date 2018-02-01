2db0ea36067a2dfb2406472d1bb84984f629851e
Casper <cmvanb@gmail.com>
2018-02-01 16:00:03 +0100

    removed submodule files from this repo, submodule setup is now mandatory

--- src/Assets/Submodules/UnityCommon/Scripts/Mono/Editor/MonoBehaviourEditor.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEditor;
-
-namespace AltSrc.UnityCommon.Mono
-{
-    [CanEditMultipleObjects]
-    [CustomEditor(typeof(MonoBehaviour), true)]
-    public class MonoBehaviourEditor : Editor {}
-}
